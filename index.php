d712807146008aeee8210105cc41f11a2e48e484
marineco <marinecolonge@gmail.com>
2017-01-16 15:15:42 +0100

    creation contenu fonction uglify, pour cumuler les fonctions bold, italic et underline, et getString, pr returner str, afin d'afficher le cumul des styles

--- index.php
+++ index.php
@@ -8,30 +8,35 @@
 			$this->str = $newStr;
 		}
 		
+		public function getString() {
+			return $this->str;
+		}
+
 		public function bold() {
 			$bold = '<strong>' .$this->str. '</strong>';
-			return $bold;
+			return $this->str = $bold;
 		}
 
 		public function italic() {
 			$italic = '<em>' .$this->str. '</em>';
-			return $italic;
+			return $this->str = $italic;
 		}
 
 		public function underline() {
 			$underline = '<u>'.$this->str. '</u>';
-			return $underline;
+			return $this->str = $underline;
 		}
 
 		public function capitalize() {
-			$underline = ucfirst($this->str);
-			return $underline;
+			$capitalize = ucfirst($this->str);
+			return $capitalize;
 		}
 
 		public function uglify() {
-
+			$this->bold();
+			$this->italic();
+			$this->underline();
 		}
-
 	}
 
 	$myObj = new StrUtils("php");
@@ -44,10 +49,11 @@
 </head>
 <body>
 
-	<div>Le mot est en gras : <?= $myObj->bold(); ?></div>
+<!-- 	<div>Le mot est en gras : <?= $myObj->bold(); ?></div>
 	<div>Le mot est en italic : <?= $myObj->italic(); ?></div>
 	<div>Le mot est souligné : <?= $myObj->underline(); ?></div>
-	<div>Le première lettre du mot est en capitale : <?= $myObj->capitalize(); ?></div>
+	<div>Le première lettre du mot est en capitale : <?= $myObj->capitalize(); ?></div> -->
+	<div>Cumul des styles : <?= $myObj->getString(); ?></div>
 
 </body>
 </html>
